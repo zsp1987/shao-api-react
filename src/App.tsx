5453706aed8e7f988c5b6140148f3ac597d2d89a
Shaopeng Zhang <zsp1987@gmail.com>
2021-04-13 08:08:57 -0400

    change to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,52 @@
 import {
-    Container,
-    CssBaseline,
-    Drawer,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Toolbar,
-    makeStyles,
+  Box,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Toolbar,
 } from '@material-ui/core';
 import { Email, SportsBasketball } from '@material-ui/icons';
 import React from 'react';
-import NBA from './nba/nba';
 import { Header } from './Header';
+import NBA from './nba/nba';
 
 const useStyle = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    drawer: {
-        width: '300px',
-    },
-    mainContent: {
-        position: 'fixed',
-        top: 64,
-        left: 300,
-        right: 0,
-        bottom: 0,
-        display: 'flex',
-    },
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: '300px',
+  },
 }));
 
 const App: React.FC = () => {
-    const classes = useStyle();
-    <CssBaseline />;
-    return (
-        <div className={classes.root}>
-            <Header></Header>
-            <Drawer variant="permanent" className={classes.drawer}>
-                <Toolbar />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
-                        (text, index) => (
-                            <ListItem button key={text}>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? (
-                                        <Email />
-                                    ) : (
-                                        <SportsBasketball />
-                                    )}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItem>
-                        )
-                    )}
-                </List>
-            </Drawer>
-            <div className={classes.mainContent}>
-                <NBA></NBA>
-            </div>
-        </div>
-    );
+  const classes = useStyle();
+  <CssBaseline />;
+  return (
+    <Box flex>
+      <Header></Header>
+      <Drawer variant="permanent" className={classes.drawer}>
+        <Toolbar />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Email /> : <SportsBasketball />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box>
+        <NBA></NBA>
+      </Box>
+    </Box>
+  );
 };
 
 export default App;
